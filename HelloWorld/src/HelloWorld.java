de1f2b38b43f48427d87a3941e9c83579ff1bd2f
wimax227 <wimax227@gmail.com>
2015-10-27 21:17:17 -0500

    My first check in

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("how are you" + " " + "how do you do");
+        System.out.println( new java.util.Date());
+      
+       
+
+
+	}
+
+}
