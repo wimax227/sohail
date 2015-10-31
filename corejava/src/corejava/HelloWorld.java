402eb7c4092e5c0e1015a280c0873844bfddab64
wimax227 <wimax227@gmail.com>
2015-10-31 18:12:41 -0500

    Update HelloWorld.java

--- corejava/src/corejava/HelloWorld.java
+++ corejava/src/corejava/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("How are you doing");
+		System.out.println("I am testing the checkout");
 		
 
 	}
